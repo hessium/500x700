1e89a117106b6d8599ed7379d3013110d3f6e7b8
hessium <82901961+hessium@users.noreply.github.com>
2022-04-11 21:12:48 +0300

    Add files via upload

---
+++ gulp.config.js
@@ -0,0 +1,16 @@
+
+
+module.exports = {
+  SRC_PATH: "src",
+  DIST_PATH: "docs",
+  STYLE_LIBS: ["node_modules/normalize.css/normalize.css",
+              'node_modules/swiper/swiper-bundle.min.css'],
+  JS_LIBS: [
+    "node_modules/jquery/dist/jquery.min.js",
+    "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js",
+    "node_modules/mobile-detect/mobile-detect.min.js",
+    "node_modules/jquery-touchswipe/jquery.touchSwipe.js",
+    "node_modules/swiper/swiper-bundle.min.js",
+    "node_modules/swiper/swiper-bundle.min.js.map"
+  ]
+};  
